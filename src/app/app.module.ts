12b29fb48d7686426019dc5136c61d67707da9b4
mathildebronnert <mathilde.bronnert@insa-lyon.fr>
2018-12-30 15:48:25 +0100

    Added deal details page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { HomePage, DetailPopover } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,7 +17,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    DetailPopover
   ],
   imports: [
     BrowserModule,
@@ -29,7 +30,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    DetailPopover
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, PopoverController, ModalController, ViewController, NavParams } from 'ionic-angular';
 
 @Component({
     selector: 'page-home',
@@ -12,7 +12,8 @@ export class HomePage {
             id: 1,
             name: "10% OFF",
             description: "Get a 10% off in the all restaurant",
-            restaurant: "VILDIA",
+            restaurantID: 1,
+            restaurantName: "VILDIA",
             type: "EUROPEAN FOOD",
             distance: 0.2,
             picture: "europeanfood.jpg",
@@ -22,7 +23,8 @@ export class HomePage {
             id: 2,
             name: "1 FREE PIZZA",
             description: "Buy two pizza and get one free",
-            restaurant: "PIZZERIA TOSCANA",
+            restaurantID: 2,
+            restaurantName: "PIZZERIA TOSCANA",
             type: "PIZZERIA",
             distance: 0.3,
             picture: "pizzeria.jpg",
@@ -31,8 +33,9 @@ export class HomePage {
         {
             id: 3,
             name: "1 FREE COFFEE",
-            description: "Get a 10% off in the all restaurant",
-            restaurant: "FRESHOOD",
+            description: "Buy a menu and get a free coffee",
+            restaurantID: 3,
+            restaurantName: "FRESHOOD",
             type: "HEALTHY FOOD",
             distance: 1.0,
             picture: "healthyfood.jpg",
@@ -41,17 +44,94 @@ export class HomePage {
         {
             id: 4,
             name: "15% OFF",
-            description: "Get a 10% off on all the pastas",
-            restaurant: "STARMA",
+            description: "Get a 15% off on all the pastas",
+            restaurantID: 4,
+            restaurantName: "STARMA",
             type: "ITALIAN FOOD",
             distance: 1.2,
             picture: "italianfood.jpg",
             labels: []
-        }
+        },
+        {
+            id: 5,
+            name: "1 FREE COFFEE",
+            description: "Buy a menu and get a free coffee",
+            restaurantID: 1,
+            restaurantName: "VILDIA",
+            type: "EUROPEAN FOOD",
+            distance: 0.2,
+            picture: "europeanfood.jpg",
+            labels: []
+        },
     ]
 
-    constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public popoverCtrl: PopoverController, public modalCtrl: ModalController) {
 
     }
 
+    viewDetails(deal) {
+        const popover = this.popoverCtrl.create(DetailPopover, { dealID: deal.id, restaurantID: deal.restaurantID, dealsList: this.dealsList });
+        popover.present();
+    }
+
+}
+
+@Component({
+    selector: 'detail-popover',
+    templateUrl: 'detail-popover.html'
+})
+export class DetailPopover {
+
+    restaurantList = [
+        {
+            restaurantID: 1,
+            restaurantName: "VILDIA",
+            restaurantDescription: "to be written",
+            environment: "Cosy, Romantic"
+        },
+        {
+            restaurantID: 2,
+            restaurantName: "PIZZERIA TOSCANA",
+            restaurantDescription: "Pizerria Toscana is the ultimate Italian experience for friends and family to share pizzas.",
+            environment: "Cosy, Familiar"
+        },
+        {
+            restaurantID: 3,
+            restaurantName: "FRESHOOD",
+            restaurantDescription: "to be written",
+            environment: "Cosy"
+        },
+        {
+            restaurantID: 4,
+            restaurantName: "STARMA",
+            restaurantDescription: "to be written",
+            environment: "Familiar, Romantic"
+        },
+    ];
+
+    dealsList;
+    restaurant;
+
+    constructor(public viewCtrl: ViewController, params: NavParams) {
+        this.restaurant = this.getRestaurant(params.get('restaurantID'));
+        this.dealsList = params.get('dealsList').filter(deal => deal.restaurantID == this.restaurant.restaurantID);
+    }
+
+    getRestaurant(restaurantID) {
+        return this.restaurantList.find((restaurant) => {
+            return restaurantID == restaurant.restaurantID;
+        })
+    }
+
+    getDeals(restaurantID) {
+        return this.dealsList;
+    }
+
+    openGoogleMaps() {
+        //TODO: redirect to Google Maps
+    }
+
+    dismiss() {
+    this.viewCtrl.dismiss();
+  }
 }
